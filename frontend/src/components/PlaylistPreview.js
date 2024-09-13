d86ac3b7c4916c61ce6d49c63980262e42694fe6
CARLOSJUMA <u22602047@tuks.co.za>
2024-09-13 03:08:40 +0200

    displaying songs and playlists on homePage feed

--- frontend/src/components/PlaylistPreview.js
+++ frontend/src/components/PlaylistPreview.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react';
 
 class PlaylistPreview extends Component {
   render() {
-    const { name, description, songs } = this.props;
+    const { name, description} = this.props;
     return (
       <div className="playlist-preview">
         <h3>{name}</h3>
         <p>{description}</p>
-        <ul>
-          {songs.map((song, index) => (
-            <li key={index}>{song.title} by {song.artist}</li>
-          ))}
-        </ul>
       </div>
     );
   }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,77 @@
 import React from 'react';
-import PlaylistPreview from '../components/PlaylistPreview';
+import PlaylistPreview from '../components/PlaylistPreview'; 
+import Song from '../components/Song';
+import Header from '../components/Header';
+const playlists = [
+    {
+      id: 1,
+      name: 'Coding Playlist',
+      description: 'Songs to listen to while coding',
+    },
+    {
+      id: 2,
+      name: 'Running Playlist',
+      description: 'Songs to listen to while running',
+    },
+    {
+      id: 3,
+      name: 'Another Playlist',
+      description: 'Songs to listen to while doing Something',
+    },
+    {
+      id: 4,
+      name: 'Another Playlist',
+      description: 'Songs to listen to while doing Something',
+    },
+    {
+      id: 5,
+      name: 'Another Playlist',
+      description: 'Songs to listen to while doing Something',
+    },
+    {
+      id: 6,
+      name: 'Another Playlist',
+      description: 'Songs to listen to while doing Nothing',
+    },
+  ];
+
+const songs = [
+    { title: 'Song 1', artist: 'Artist 1' },
+    { title: 'Song 2', artist: 'Artist 2' },
+    { title: 'Song 3', artist: 'Artist 3' },
+    { title: 'Song 4', artist: 'Artist 4' },
+    { title: 'Song 5', artist: 'Artist 5' }
+  ];
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playlists: []
+      playlists: [],
+      songs: []
     };
   }
 
-  async componentDidMount() {
-    const response = await fetch('/api/playlists');
-    const playlists = await response.json();
-    this.setState({ playlists });
+  componentDidMount() {
+    this.setState({ playlists, songs });
   }
 
   render() {
     return (
       <div>
-        <h1>Playlists</h1>
+        <Header />
+        <h1>Playlists Feed</h1>
         {this.state.playlists.map((playlist, index) => (
-          <PlaylistPreview key={index} {...playlist} />
+          <PlaylistPreview key={index} {...playlist}/>
+        ))}
+
+        <h1>Songs Feed</h1>
+        {this.state.songs.map((song, index) => (
+          <Song key={index} {...song}/>
         ))}
       </div>
     );
   }
 }
+
+export default Home;
